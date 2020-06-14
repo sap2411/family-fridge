eca4446c3f6e252dc1b7db2d0d2a2b2b37e3f517
sap2411 <steven.parsons.us@gmail.com>
2020-06-14 09:49:59 -0700

    sketch out ages

--- pages/account.js
+++ pages/account.js
@@ -1,2 +1,14 @@
-let accountPage = `<h1>Account Page</h1>
-<h2>Username</h2>`
+let accountPage = `
+<h1>Account Page</h1>
+<h2>Username</h2>
+
+<h3>Your Photos:</h3>
+<ul id="photo-album"><div>
+
+</div></ul>
+
+<h3>Your Fridges<h3>
+<ul id="fridge-list"><div>
+
+</div></ul>
+`
---
+++ pages/upload_img.js
@@ -0,0 +1,8 @@
+let uploadImg = `
+<h1>Upload An Image</h1>
+<form action="/action_page.php"> <<----ignore that action address
+  <label for="img">Select image:</label>
+  <input type="file" id="img" name="img" accept="image/*">
+  <input type="submit">
+</form>
+`
