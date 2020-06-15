e1ef310f0d807f15b8949a4e3f6c7425e5429700
sap2411 <steven.parsons.us@gmail.com>
2020-06-14 19:53:47 -0700

    connect pages

--- pages/new_fridge_form.js
+++ pages/new_fridge_form.js
@@ -1,9 +1,50 @@
-let newFridgePage = `
+function newFridgePage(){ 
+return `
 <h1>Make A New Fridge</h1>
 <form>
-<input name='name' >
-<collection-select select friends>
+<input type='text' placeholder='Fridge Name' name='name'>
+<input list="friends" placeholder='Add a friend'>
+  <datalist id="friends">
+    <option value="Aidan">
+    <option value="Steven">
+    <option value="Erin">
+    <option value="Soundarya">
+    <option value="Colton">
+  </datalist>
+  <input list="friends" placeholder='Add a friend'>
+  <datalist id="friends2">
+    <option value="Aidan">
+    <option value="Steven">
+    <option value="Erin">
+    <option value="Soundarya">
+    <option value="Colton">
+  </datalist>
+  <input list="friends" placeholder='Add a friend'>
+  <datalist id="friends3">
+    <option value="Aidan">
+    <option value="Steven">
+    <option value="Erin">
+    <option value="Soundarya">
+    <option value="Colton">
+  </datalist>
+  <input list="friends" placeholder='Add a friend'>
+  <datalist id="friends4">
+    <option value="Aidan">
+    <option value="Steven">
+    <option value="Erin">
+    <option value="Soundarya">
+    <option value="Colton">
+  </datalist>
+  <input list="friends" placeholder='Add a friend'>
+  <datalist id="friends5">
+    <option value="Aidan">
+    <option value="Steven">
+    <option value="Erin">
+    <option value="Soundarya">
+    <option value="Colton">
+  </datalist><br>
 
-<input name='submit' value='submit'>
+<input type='submit' name='submit'>
 </form>
 `
+}
--- pages/upload_img.js
+++ pages/upload_img.js
@@ -1,8 +1,10 @@
-let uploadImg = `
+function uploadImgPage(){
+return `
 <h1>Upload An Image</h1>
-<form action="/action_page.php"> <<----ignore that action address
+<form action="/action_page.php">
   <label for="img">Select image:</label>
   <input type="file" id="img" name="img" accept="image/*">
   <input type="submit">
 </form>
 `
+}
