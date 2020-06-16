13276f8f56147ba75b1d28148589679835543c3c
sap2411 <steven.parsons.us@gmail.com>
2020-06-16 12:58:36 -0700

    desciption

--- pages/upload_img.js
+++ pages/upload_img.js
@@ -4,6 +4,7 @@ return `
 <form action="/action_page.php">
   <label for="img">Select image:</label>
   <input type="file" id="img-input" name="img" accept="image/*">
+  <textarea id="img-description" name="description" rows="4" cols="50" mexlength="100" placeholder="Enter Image Description">
   <input type="submit">
 </form>
 `
