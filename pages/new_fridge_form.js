0f4fec6cc86657718c2483384a3533e1d56a3c0a
sap2411 <steven.parsons.us@gmail.com>
2020-06-16 18:19:42 -0700

    fridge post

--- pages/new_fridge_form.js
+++ pages/new_fridge_form.js
@@ -3,19 +3,19 @@ return `
 <h1>Make A New Fridge</h1>
 <form id="new_fridge_form">
 <input type="text" name="title" placeholder="Fridge Name"><br>
-<select name="friends" id="friends1">
+<select name="friends1" id="friends1">
   <option value="null">Select a Friend</option>
 </select><br>
-<select name="friends" id="friends2">
+<select name="friends2" id="friends2">
   <option value="null">Select a Friend</option>
 </select><br>
-<select name="friends" id="friends3">
+<select name="friends3" id="friends3">
   <option value="null">Select a Friend</option>
 </select><br>
-<select name="friends" id="friends4">
+<select name="friends4" id="friends4">
   <option value="null">Select a Friend</option>
 </select><br>
-<select name="friends" id="friends5">
+<select name="friends5" id="friends5">
   <option value="null">Select a Friend</option>
 </select><br>
 <input type='submit' name='submit' class="btn btn-primary">
