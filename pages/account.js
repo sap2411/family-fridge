457b698d28412cd377a973525503acefb7ab725e
aidanmc95 <aidanmc95@gmail.com>
2020-06-18 13:22:08 -0700

    Styling of images

--- pages/account.js
+++ pages/account.js
@@ -6,12 +6,12 @@ function accountPage(){
 <h3>Your Photos</h3>
 <div id="photo-collection" class="album">
 
-</div>
+</div><br><br>
 
 <h3>Your Fridges</h3>
 <div id="fridge-collection" class="album">
 
-</div><br>
+</div><br><br>
 <button id='delete-account' class="btn btn-primary" type='button'>Delete Account</button>
 `
 }
